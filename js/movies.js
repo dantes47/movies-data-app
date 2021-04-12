c7a46511f02cf096290aa6cbc1f16ac9b7e9e3a8
Danny <dantes817@gmail.com>
2021-04-12 18:52:07 +0200

    Initial commit

---
+++ js/movies.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// 1
+const numberOfFilms = +prompt('How many movies do you already looked?', '');
+
+// 2
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+// 3 >> two times need to be ask..
+let i = 0, fstAnswer, sndAnswer;
+
+do {
+      fstAnswer = prompt('One of your last looked movies?', ''),
+      sndAnswer = +prompt('What score do you give that movie?', '');
+      personalMovieDB.movies[fstAnswer] = sndAnswer;
+      ++i;
+
+      } while (i <= 1);
+
+
+console.log(personalMovieDB);
