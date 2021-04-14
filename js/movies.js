ccad11db3c89fccb50f5a0a5cc47c214ce8a094d
Danny <dantes817@gmail.com>
2021-04-14 22:00:37 +0200

    was tested with two cycles

--- js/movies.js
+++ js/movies.js
@@ -13,15 +13,63 @@ const personalMovieDB = {
 };
 
 // 3 >> two times need to be ask..
-let i = 0, fstAnswer, sndAnswer;
+// 1st way with do-while:
 
-do {
-      fstAnswer = prompt('One of your last looked movies?', ''),
-      sndAnswer = +prompt('What score do you give that movie?', '');
-      personalMovieDB.movies[fstAnswer] = sndAnswer;
-      ++i;
+// let i = 0, fstAnswer, sndAnswer;
 
-      } while (i <= 1);
+// do {
+//       fstAnswer = prompt('One of your last looked movies?', '');
+//       sndAnswer = +prompt('What score do you give that movie?', '');
+//       if (fstAnswer != null && sndAnswer != null &&
+//           fstAnswer != '' && sndAnswer != '' &&
+//           fstAnswer.length < 50) {
 
-// result in browser console..
-console.log(personalMovieDB);
+//             personalMovieDB.movies[fstAnswer] = sndAnswer;
+//             console.log();
+//       } else {
+//         console.log('Error..');
+//         --i;
+//       }
+//         ++i;
+
+//       } while (i <= 1);
+
+// // result in browser console..
+// console.log(personalMovieDB);
+
+// 2nd way with for:
+
+for (let i = 0; i <= 1; ++i) {
+
+    const fstAnswer = prompt('One of your last looked movies?', ''),
+          sndAnswer = +prompt('What score do you give that movie?', '');
+
+    if (fstAnswer !== '' && fstAnswer !== null && fstAnswer.length < 50 &&
+        sndAnswer !== '' && sndAnswer !== null) {
+
+        personalMovieDB.movies[fstAnswer] = sndAnswer;
+        console.log('Done!');
+
+    } else {
+
+        console.log('Error..');
+        --i;
+
+    }
+}
+
+console.log(personalMovieDB.movies);
+
+if (numberOfFilms < 10) {
+    console.log('Not many movies was watched..');
+
+} else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
+
+    console.log('You\'re normal, regular viewer..');
+} else if (numberOfFilms > 30) {
+
+    console.log('You\'re movie-maniak, my dear!')
+} else {
+
+    console.log('Some Error.. Check your code..');
+}
